c1ed32ac4c633f800f4fbae223f4fc8526b38567
Adr <152195399+DroidWizardNyxx@users.noreply.github.com>
2025-04-07 09:03:47 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,36 @@
 import { findByProps, findByStoreName } from '@vendetta/metro';
 import { ReactNative } from '@vendetta/metro/common';
-import { after, before, instead } from '@vendetta/patcher';
-import { CDN_URL, RAW_SKU_ID, SKU_ID } from './lib/constants';
 import Settings from './ui/pages/Settings';
-import { subscriptions as CurrentUserDecorationsStoreSubscriptions } from './lib/stores/CurrentUserDecorationsStore';
-import { subscriptions as UserDecorationsStoreSubscriptions, useUsersDecorationsStore } from './lib/stores/UsersDecorationsStore';
-import { unsubscribe } from './lib/stores/AuthorizationStore';
-
-const UserStore = findByStoreName('UserStore');
 const ImageResolver = findByProps('getAvatarDecorationURL', 'default');
 const AvatarDecorationUtils = findByProps('isAnimatedAvatarDecoration');
 
-let patches = [];
+const initVendettaPlugin = async () => {
+    await Promise.all([
+        initThemes(),
+        injectFluxInterceptor(),
+        patchSettings(),
+        patchLogHook(),
+        patchCommands(),
+        patchJsx(),
+        initVendettaObject(),
+        initFetchI18nStrings(),
+        initSettings(),
+        initFixes(),
+        patchErrorBoundary(),
+        updatePlugins()
+    ]).then(
+        u => u.forEach(f => f && lib.unload.push(f))
+    );
 
-export default {
-	onLoad: async () => {
-		patches.push(unsubscribe);
-		patches.push(...UserDecorationsStoreSubscriptions);
-		patches.push(...CurrentUserDecorationsStoreSubscriptions);
-		
-		patches.push(
-			after('getUser', UserStore, (_, user) => {
-				const store = useUsersDecorationsStore.getState();
+    window.bunny = lib;
 
-				if (user && store.has(user.id)) {
-					const decoration = store.get(user.id);
-		
-					if (decoration && user.avatarDecoration?.skuId !== SKU_ID) {
-						user.avatarDecoration = {
-							asset: decoration,
-							skuId: SKU_ID
-						};
-					} else if (!decoration && user.avatarDecoration && user.avatarDecoration?.skuId === SKU_ID) {
-						user.avatarDecoration = null;
-					}
-		
-					user.avatarDecorationData = user.avatarDecoration;
-				}
-			})
-		);
+    VdPluginManager.initPlugins()
+        .then(u => lib.unload.push(u))
+        .catch(() => alert("Failed to initialize Vendetta plugins"));
 
-		patches.push(
-			instead('getAvatarDecorationURL', ImageResolver, (args, orig) => {
-				const [{avatarDecoration, canAnimate}] = args;
-				if (avatarDecoration?.skuId === SKU_ID) {
-					const parts = avatarDecoration.asset.split("_");
-					if (!canAnimate && parts[0] === "a") parts.shift();
-					return CDN_URL + `/${parts.join("_")}.png`;
-				} else if (avatarDecoration?.skuId === RAW_SKU_ID) {
-					return avatarDecoration.asset;
-				} else {
-					return orig(...args);
-				}
-			})
-		);
-
-		patches.push(
-			after('isAnimatedAvatarDecoration', AvatarDecorationUtils, ([avatarDecoration], _) => {
-				if (ReactNative.Platform.OS === 'ios' && avatarDecoration?.asset?.startsWith('file://')) return true;
-			})
-		);
-
-		useUsersDecorationsStore.getState().fetch(UserStore.getCurrentUser().id, true);
-	},
-	onUnload: () => {
-		patches.forEach((unpatch) => unpatch());
-	},
-	settings: Settings
+    initPlugins();
+    updateFonts();
+    logger.log("Vendetta Plugin is ready!");
 };
+
+export default initVendettaPlugin;
